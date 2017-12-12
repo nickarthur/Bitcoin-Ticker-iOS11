63f267f94bc8cf07cbdbedeba65fb8b4c28ba2e5
Larry Nickerson <lnickers@austin.rr.com>
2017-12-11 22:51:21 -0600

    added currency symbols and adjusted attributed text

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -8,18 +8,21 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-   
     
-    //MARK: ATTRIBUTED STRING STYLING
-    //BEGIN LEN DEBUG
-    // Create the attributed string
-    var myString = NSMutableAttributedString(string:"AUD")
     
     //MARK: PROPERTIES
+    /***************************************************************/
+
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    
+    // NOTE: the followig two arrays must be kept in sync
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let symbolsArray =    ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    
     var finalURL = ""
 
     //MARK: OUTLETS
@@ -29,6 +32,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
 
     //MARK: Methods
+    /***************************************************************/
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,6 +46,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     //TODO: Place your 3 UIPickerView delegate methods here
     
     //MARK: UIPickerViewDatasource Methods
+    /***************************************************************/
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1 // only 1 column of data
     }
@@ -50,19 +57,22 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     //MARK: UIPickerViewDelegate Delegate Methods
+    /***************************************************************/
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return currencyArray[row]
+        return symbolsArray[row] + currencyArray[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
 
         finalURL = baseURL + currencyArray[row]
         print(finalURL)
+        getCurrencyData(url: finalURL)
     }
     
     func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
         
-        let myString = NSMutableAttributedString(string: currencyArray[row])
+        let myString = NSMutableAttributedString(string: symbolsArray[row] + currencyArray[row])
         
         // Declare the fonts
         let myStringFont1 = UIFont(name:"Helvetica-Light", size:30.0)!
@@ -77,57 +87,52 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         myStringParaStyle1.tabStops = [NSTextTab(textAlignment: NSTextAlignment.left, location: 28.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 56.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 84.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 112.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 140.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 168.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 196.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 224.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 252.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 280.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 308.000000, options: [:]), NSTextTab(textAlignment: NSTextAlignment.left, location: 336.000000, options: [:]), ]
         
         
+        let length = myString.length
         // Create the attributes and add them to the string
-        myString.addAttribute(NSAttributedStringKey.foregroundColor, value:myStringColor1, range:NSMakeRange(0,3))
-        myString.addAttribute(NSAttributedStringKey.paragraphStyle, value:myStringParaStyle1, range:NSMakeRange(0,3))
-        myString.addAttribute(NSAttributedStringKey.font, value:myStringFont1, range:NSMakeRange(0,3))
+        myString.addAttribute(NSAttributedStringKey.foregroundColor, value:myStringColor1, range:NSMakeRange(0,length))
+        myString.addAttribute(NSAttributedStringKey.paragraphStyle, value:myStringParaStyle1, range:NSMakeRange(0,length))
+        myString.addAttribute(NSAttributedStringKey.font, value:myStringFont1, range:NSMakeRange(0,length))
 
             return myString
     }
     
-//    
-//    //MARK: - Networking
-//    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
-//    
-//    
-//    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    func getCurrencyData(url: String) {
+        
+        Alamofire.request(url, method: .get, parameters: nil)
+            .responseJSON { response in
+                if response.result.isSuccess {
+
+                    print("Sucess! Got the currency data")
+                    let currencyJSON : JSON = JSON(response.result.value!)
+
+                    self.updateCurrencyData(json: currencyJSON)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+
+    }
+
+    
+    
+    
+    
 //    //MARK: - JSON Parsing
-//    /***************************************************************/
-//    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
+    /***************************************************************/
+    
+    func updateCurrencyData(json : JSON) {
+        
+        print(json["averages"]["day"])
+        bitcoinPriceLabel.text = json["averages"]["day"].stringValue
+        
+    }
+    
 
 
 
